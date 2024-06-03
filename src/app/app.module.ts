f248ce170cb2e7058ed7c708abb252e03912fd35
Hector <hector.gonzalez2@soprasteria.com>
2024-06-03 11:51:20 +0200

    Added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './gifs/pages/home/home-page.component';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
